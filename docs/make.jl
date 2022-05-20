20dbdf1e949a0eb39a76fdc0d4ff2b40700c1243
Andreas Dominik <andreas.dominik@mni.thm.de>
2022-05-20 14:35:47 +0200

    make.jl test

--- docs/make.jl
+++ docs/make.jl
@@ -22,7 +22,7 @@ deploydocs(
     # make   = nothing | <Function>,
     devbranch = "main",
     # devurl = "dev",
-    versions = ["stable" => "v^", "v#.#", devurl => "dev"],
+    versions = ["stable" => "v^", "v#.#", "dev" => "dev"],
     # push_preview    = false,
     # repo_previews   = repo,
     # branch_previews = branch
